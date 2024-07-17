d301b5940a102b8f32bda002e0016811c88aca47
Edgar Lopez <edgarlopez755@gmail.com>
2024-07-16 22:08:43 -0700

    Exercise 8 done

--- app.js
+++ app.js
@@ -117,3 +117,18 @@ console.log(game.party)
 
 
 
+
+/*
+Exercise 8
+1. Print the name of each Pokémon in your party.
+2. Consider using a loop or an array method to access each Pokémon's name.
+
+Solve Exercise 8 here:
+*/
+
+
+
+game.party.forEach(party => {
+    console.log(party.name)
+});
+
